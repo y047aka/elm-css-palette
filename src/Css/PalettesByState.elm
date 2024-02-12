8b6d32313771f9c25c49f9b19952b29a5992509b
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2024-02-13 02:59:28 +0900

    chore: remove unnecessary functions

--- src/Css/PalettesByState.elm
+++ src/Css/PalettesByState.elm
@@ -1,14 +1,12 @@
 module Css.PalettesByState exposing
     ( PalettesByState, initPalettes
     , palettesByState
-    , palettesByStateWithBackground, palettesByStateWithColor, palettesByStateWithBorder
     )
 
 {-|
 
 @docs PalettesByState, initPalettes
 @docs palettesByState
-@docs palettesByStateWithBackground, palettesByStateWithColor, palettesByStateWithBorder
 
 -}
 
@@ -60,64 +58,3 @@ palettesByState ps =
     ]
         |> List.filterMap identity
         |> Css.batch
-
-
-type alias Options color =
-    { background : color -> Style
-    , color : color -> Style
-    , border : color -> Style
-    }
-
-
-palettesByStateWith : Options color -> PalettesByState color -> Style
-palettesByStateWith fn ps =
-    [ case ps.selected of
-        Just ( True, selected ) ->
-            Just (paletteWith fn selected)
-
-        _ ->
-            Just (paletteWith fn ps.default)
-    , Maybe.map (\p -> Css.link [ paletteWith fn p ]) ps.link
-    , Maybe.map (\p -> Css.visited [ paletteWith fn p ]) ps.visited
-    , Maybe.map (\p -> Css.hover [ paletteWith fn p ]) ps.hover
-    , Maybe.map (\p -> Css.focus [ paletteWith fn p ]) ps.focus
-    , Maybe.map (\p -> Css.active [ paletteWith fn p ]) ps.active
-    ]
-        |> List.filterMap identity
-        |> Css.batch
-
-
-paletteWith : Options color -> Palette color -> Style
-paletteWith fn p =
-    [ Maybe.map fn.background p.background
-    , Maybe.map fn.color p.color
-    , Maybe.map fn.border p.border
-    ]
-        |> List.filterMap identity
-        |> Css.batch
-
-
-default : Options (ColorValue c)
-default =
-    { background = Css.backgroundColor
-    , color = Css.color
-    , border = Css.borderColor
-    }
-
-
-{-| -}
-palettesByStateWithBackground : (ColorValue c -> Style) -> PalettesByState (ColorValue c) -> Style
-palettesByStateWithBackground fn =
-    palettesByStateWith { default | background = fn }
-
-
-{-| -}
-palettesByStateWithColor : (ColorValue c -> Style) -> PalettesByState (ColorValue c) -> Style
-palettesByStateWithColor fn =
-    palettesByStateWith { default | color = fn }
-
-
-{-| -}
-palettesByStateWithBorder : (ColorValue c -> Style) -> PalettesByState (ColorValue c) -> Style
-palettesByStateWithBorder fn =
-    palettesByStateWith { default | border = fn }
